272754b8e555e61396acc68ea53cae53add95176
Guzman Monne <guzmonne@hotmail.com>
2014-09-27 19:51:15 -0300

    first commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('convideoApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
